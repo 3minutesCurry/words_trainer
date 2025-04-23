20a9ea7fb5a92010545a32d64b2e57c699cf9dd5
shin <dlehdrjs98@naver.com>
2025-04-23 17:11:36 +0900

    random function added

--- main.py
+++ main.py
@@ -108,10 +108,33 @@ elif st.session_state["step"] == 3:
         st.session_state["perm_dict_length"] = len(dict_list[dic_num].keys())
         dict_length = st.session_state["dict_length"]
 
-    st.session_state["step"] = 4
     st.session_state["now_word_number"] = 1
-    
-    st.rerun()
+
+    if the_file.type == "word":
+        # 3등분된 열 생성
+        col1, col2, col3, col4 = st.columns(4)
+
+        # 각 열에 버튼 배치
+        with col1:
+            if st.button("기본", use_container_width=True):
+                st.session_state["word_type"] = 0
+                st.session_state["step"] = 4
+                st.rerun()
+        with col2:
+            if st.button("단어만", use_container_width=True):
+                st.session_state["step"] = 4
+                st.session_state["word_type"] = 1
+                st.rerun()
+        with col3:
+            if st.button("히라가나만", use_container_width=True):
+                st.session_state["step"] = 4
+                st.session_state["word_type"] = 2
+                st.rerun()
+        with col4:
+            if st.button("랜덤", use_container_width=True):
+                st.session_state["step"] = 4
+                st.session_state["word_type"] = 3
+                st.rerun()
 
 
 elif st.session_state["step"] == 4:
@@ -124,18 +147,39 @@ elif st.session_state["step"] == 4:
     dict_length = st.session_state["dict_length"]
     now_word_number = st.session_state["now_word_number"]
     perm_dict_length = st.session_state["perm_dict_length"]
+    word_type = st.session_state["word_type"]
 
     if perm_dict_length + 1 == now_word_number:
         st.session_state["step"] = 6
         st.rerun()
+
 
     words_random_int = random.randrange(0, len(dict_keys))
     now_key = dict_keys[words_random_int]
 
     if the_file.type == "word":
         if "(" in now_key:
-            head, tail = now_key.split("(", 1) 
-            tail = "<span style='display: inline-block; max-width: 100%; word-break: break-word;'>(" +tail + "</span>"
+            if word_type == 0:
+                head, tail = now_key.split("(", 1) 
+                tail = "<span style='display: inline-block; max-width: 100%; word-break: break-word;'>(" +tail + "</span>"
+            elif word_type == 1:
+                head, tail = now_key.split("(", 1) 
+                tail = ""
+            elif word_type == 2:
+                head, tail = now_key.split("(", 1) 
+                head = ""
+                tail = tail[:-1]
+                tail = "<span style='display: inline-block; max-width: 100%; word-break: break-word;'>" +tail + "</span>"
+            elif word_type == 3:
+                temp = random.choice([0, 1])
+                if temp == 0:
+                    head, tail = now_key.split("(", 1) 
+                    tail = ""
+                elif temp == 1:
+                    head, tail = now_key.split("(", 1) 
+                    head = ""
+                    tail = tail[:-1]
+                    tail = "<span style='display: inline-block; max-width: 100%; word-break: break-word;'>" +tail + "</span>"
         else:
             head = now_key
             tail = ""
@@ -147,6 +191,7 @@ elif st.session_state["step"] == 4:
         st.markdown(f'<p style="text-align:center; font-size:20px;">[ {the_file.name} 전체 ]</p>', unsafe_allow_html=True)
     else:
         st.markdown(f'<p style="text-align:center; font-size:20px;">[ {the_file.name}   {dic_num+1} ]</p>', unsafe_allow_html=True)
+
 
 
     st.markdown(f'<p style="text-align:center; font-size:30px;">{now_word_number}/{perm_dict_length}</p>', unsafe_allow_html=True)
